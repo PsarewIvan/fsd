8813c760cea98b488f3b43315538fb8d8644b18a
Ivan Psarew <psarew.ivan@yandex.ru>
2020-07-07 22:07:37 +0300

    Заводит базовую структуру

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import $ from 'jquery';
-import Post from './post.js';
-import './styles/styles.css';
-import json from './assets/json.json';
-import image from './assets/picture.jpg';
-
-const post = new Post('New Post', image);
-console.log('Post: ', post.toString());
-
-console.log('Hello, world');
-
-console.log('JSON:', json);
-
-$('pre').html(post.toString());
+import './assets/scss/styles.scss';
