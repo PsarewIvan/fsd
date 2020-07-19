948646345d53f4fc1f205f937f2202686484689d
Ivan Psarew <psarew.ivan@yandex.ru>
2020-07-19 22:54:48 +0300

    Делает компонент dropdown более доступным

--- src/pug/includes/dropdown/dropdown.js
+++ src/pug/includes/dropdown/dropdown.js
@@ -1,15 +1,61 @@
 (function () {
-  const dropdown = document.querySelectorAll('.dropdown');
+  const dropdowns = document.querySelectorAll('.dropdown');
 
-  Array.prototype.forEach.call(dropdown, heading => {
-    let btn = heading.querySelector('.dropdown__drop-button');
-    let target = heading.querySelector('.dropdown__menu');
+  function replaceTag(element, newTag) {
+    let elementNew = document.createElement(newTag);
+    elementNew.innerHTML = element.innerHTML;
+
+    Array.prototype.forEach.call(element.attributes, attr => {
+      elementNew.setAttribute(attr.name, attr.value);
+    });
+
+    element.parentNode.insertBefore(elementNew, element);
+    element.parentNode.removeChild(element);
+    return elementNew;
+  }
+
+  function createBtn(aria, content) {
+    let btnElem = document.createElement('button');
+    btnElem.classList.add('dropdown__button');
+    btnElem.setAttribute('type', 'button');
+    btnElem.setAttribute('aria-label', aria);
+    btnElem.innerHTML = content;
+    return btnElem;
+  }
+
+  Array.prototype.forEach.call(dropdowns, dropdown => {
+    replaceTag(dropdown.querySelector('.dropdown__drop-button'), 'button');
+    let btn = dropdown.querySelector('.dropdown__drop-button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-expanded', 'false');
+    btn.innerHTML = `${btn.innerHTML}
+      <svg width="12" height="8" viewBox="0 0 12 8" aria-hidden="true" focusable="false">
+        <path d="M10.5938 0.578125L12 1.98438L6 7.98438L0 1.98438L1.40625 0.578125L6 5.17188L10.5938 0.578125Z" fill="#1F2041" fill-opacity="0.5">
+      </svg>
+    `;
+
+    let dropdownMenu = dropdown.querySelector('.dropdown__menu');
+    let dropdownInputs = dropdown.querySelectorAll('.dropdown__input');
+    dropdownMenu.hidden = true;
+
+    Array.prototype.forEach.call(dropdownInputs, dropdownInput => {
+      dropdownInput.parentNode.insertBefore(createBtn('low', '-'), dropdownInput);
+      dropdownInput.parentNode.insertBefore(createBtn('hight', '+'), dropdownInput.nextSibling);
+    });
+
+    let dropdownCtrl = document.createElement('div');
+    dropdownCtrl.classList.add('dropdown__controls');
+    dropdownCtrl.innerHTML = `
+      <button class="dropdown__button dropdown__button--clear", type="button" aria-label="Очистить количество гостей")>Очистить</button>
+      <button class="dropdown__button dropdown__button", type="button" aria-label="Применить количество гостей">Применить</button>
+    `;
+    dropdownMenu.appendChild(dropdownCtrl);
 
     btn.onclick = () => {
-      let expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+      let expanded = btn.getAttribute('aria-expanded') === 'true' || false
 
-      btn.setAttribute('aria-expanded', !expanded);
-      target.hidden = expanded;
+      btn.setAttribute('aria-expanded', !expanded)
+      dropdownMenu.hidden = expanded
     }
   })
 })()
