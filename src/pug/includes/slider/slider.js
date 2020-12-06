b63f3c59ef3d08eb48f547c2ad3ebabe45ff21f1
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-06 22:40:29 +0300

    Кастомизирует слайдер

--- src/pug/includes/slider/slider.js
+++ src/pug/includes/slider/slider.js
@@ -2,12 +2,5 @@ import 'ion-rangeslider';
 import $ from 'jquery';
 
 (function() {
-  $(".js-range-slider").ionRangeSlider({
-    type: "double",
-    min: 0,
-    max: 1000,
-    from: 200,
-    to: 500,
-    grid: true
-  });
+  $(".js-range-slider").ionRangeSlider();
 })()
