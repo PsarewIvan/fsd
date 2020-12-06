0c8902c71b23080b5b9439224650a727b607ad14
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-06 23:34:05 +0300

    Создает функциональность слайдера

--- src/pug/includes/slider/slider.js
+++ src/pug/includes/slider/slider.js
@@ -2,5 +2,24 @@ import 'ion-rangeslider';
 import $ from 'jquery';
 
 (function() {
-  $(".js-range-slider").ionRangeSlider();
+  const sliders = document.querySelectorAll(".js-slider");
+
+  Array.prototype.forEach.call(sliders, slider => {
+    const fromToValue = slider.querySelector('.slider__data-from-to');
+
+    function changeValue(data) {
+      $(fromToValue).html(`${data.from_pretty}&#8381; - ${data.to_pretty}&#8381;`);
+    }
+
+    $(".js-range-slider").ionRangeSlider({
+      onStart: function(data) {
+        changeValue(data);
+      },
+
+      onChange: function(data) {
+        changeValue(data);
+      }
+    });
+  });
+
 })()
