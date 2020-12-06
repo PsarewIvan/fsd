aafa184f59357202e2de27819dd62260334e382c
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-06 21:32:46 +0300

    Заводит стандартный слайдер

---
+++ src/pug/includes/slider/slider.js
@@ -0,0 +1,13 @@
+import 'ion-rangeslider';
+import $ from 'jquery';
+
+(function() {
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 500,
+    grid: true
+  });
+})()
