60f1129511613f88d0b0c789e269a682c3bbfefe
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-08 13:38:14 +0300

    Добавляет стандартные параметры в календарь и добавляет кнопку применить

--- src/pug/includes/calendar/calendar.js
+++ src/pug/includes/calendar/calendar.js
@@ -4,6 +4,31 @@ import 'air-datepicker';
   const datepickers = document.querySelectorAll('.calendar');
 
   Array.prototype.forEach.call(datepickers, function(datepicker) {
-    $(datepicker).datepicker();
+
+    function addApplyButton() {
+      const butonsContainer = datepicker.querySelector('.datepicker--buttons');
+      const applyButton = document.createElement('span');
+      applyButton.innerHTML = 'Применить';
+      applyButton.setAttribute('data-action', 'apply');
+      applyButton.classList.add('datepicker--button', 'datepicker--button-apply');
+      butonsContainer.appendChild(applyButton);
+
+    }
+
+    $(datepicker).datepicker({
+      minDate: new Date(),
+      range: 'true',
+      navTitles: {days: 'MM yyyy'},
+      inline: true,
+      clearButton: true,
+      onRenderCell: (data, cellType) => {
+        return {
+          html: `<span class="calendar__current-date">${data.getDate()}</span>`
+        }
+      }
+    });
+
+    addApplyButton();
+
   });
 })();
