441f00db203e310cd670826d8efed515f443399a
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-08 11:16:03 +0300

    Прячет календарь при двойном инпуте календаря

--- src/pug/includes/drop-date/drop-date.js
+++ src/pug/includes/drop-date/drop-date.js
@@ -1,10 +1,14 @@
-import 'air-datepicker';
-
 (function() {
   const dropDates = document.querySelectorAll('.drop-date');
 
   Array.prototype.forEach.call(dropDates, function(dropDate) {
     const inputsAmount = dropDate.querySelectorAll('input');
     let isMultiple = inputsAmount.length <= 1 ? false : true;
+
+    if (isMultiple) {
+      dropDate.querySelector('.js-drop-date__calendar').classList.add("display-none");
+    }
+
+
   });
 })();
