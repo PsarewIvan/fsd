7713886f163eb6a994d85d0c81254541013fba4a
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-09 19:05:37 +0300

    Правит код, добавляет функциональность на кнопку "применить"

--- src/pug/includes/calendar/calendar.js
+++ src/pug/includes/calendar/calendar.js
@@ -4,12 +4,11 @@ import 'air-datepicker';
   const datepickers = document.querySelectorAll('.calendar');
 
   Array.prototype.forEach.call(datepickers, function(datepicker) {
-
     function addApplyButton() {
       const element = createElement(
         'span',
         ['datepicker--button-apply', 'js-datepicker--button-apply'],
-        [{ name: 'data-action', value: 'apply' }],
+        [{name: 'data-action', value: 'apply'}, {name: 'data-disabled', value: 'true'}],
         'Применить'
       );
       datepicker.querySelector('.datepicker--buttons').appendChild(element);
@@ -33,18 +32,22 @@ import 'air-datepicker';
     }
 
     function applyDates() {
-      const dropDate = datepicker.closest('.drop-date');
-      const dateInputs= dropDate.querySelectorAll('.js-drop-date__input');
-
       dpData.selectedDates.forEach((date, index) => {
         dateInputs[index].value = parseDate(date);
       });
     }
 
     function resetDates() {
-      const dropDate = datepicker.closest('.drop-date');
-      const dateInputs = dropDate.querySelectorAll('.js-drop-date__input');
+      dateInputs.forEach(function(input, index) {
+        input[index].value = '';
+      })
+    }
 
+    function changeElementDataState(element, data, state) {
+      element.setAttribute(`data-${data}`, state);
+    }
+
+    function resetDates() {
         Array.prototype.forEach.call(dateInputs, function(input) {
           input.value = '';
         })
@@ -67,11 +70,25 @@ import 'air-datepicker';
     });
 
     const dpData = $(datepicker).data('datepicker');
+
     addApplyButton();
+
     const applyButton = datepicker.querySelector('.js-datepicker--button-apply');
     const resetButton = datepicker.querySelector('span[data-action=clear]');
+    const dropDate = datepicker.closest('.drop-date');
+    const dateInputs = dropDate.querySelectorAll('.js-drop-date__input');
 
-    applyButton.addEventListener('click', applyDates);
+    applyButton.addEventListener('click', () => {
+      applyDates();
+      if (dpData.selectedDates.length === 2) {
+        changeElementDataState(applyButton, 'disabled', 'false');
+      }
+      else {
+        resetDates();
+        applyDates();
+        changeElementDataState(applyButton, 'disabled', 'true');
+      }
+    });
     resetButton.addEventListener('click', resetDates);
   });
 })();
