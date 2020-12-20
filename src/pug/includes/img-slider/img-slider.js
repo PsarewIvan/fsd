3e40778e51f35343ac76c991ba43004fad4fbde4
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-20 23:09:09 +0300

    Делает слайдеру кастомные кнопки

--- src/pug/includes/img-slider/img-slider.js
+++ src/pug/includes/img-slider/img-slider.js
@@ -4,7 +4,9 @@ import 'slick-carousel';
   $(document).ready(function () {
     $('.img-slider').slick({
       dots: true,
-      infinite: true
+      infinite: true,
+      nextArrow: "<button type='button' class='slick-next' aria-label='Next'></button>",
+      prevArrow: "<button type='button' class='slick-prev' aria-label='Previous'></button>",
     });
   });
 })();
