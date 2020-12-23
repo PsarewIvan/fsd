88ead610f7db39e33f49fe67d8c15a1f11c79c7b
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-24 02:52:42 +0300

    Добавляет логику выпадающего списка для тач-устройств

--- src/pug/includes/nav/nav.js
+++ src/pug/includes/nav/nav.js
@@ -1,9 +1,46 @@
 (function() {
   const navigations = document.querySelectorAll('.nav');
-  Array.prototype.forEach.call(navigations, nav => {
+
+    navigations.forEach( (nav) => {
+    // Убирает атрибут href у текущей ссылки
     if ( nav.querySelector('.nav__link--current') ) {
       const  navLink = nav.querySelector('.nav__link--current');
       navLink.removeAttribute('href');
+    }
+
+    // Проверка на тач-устройства
+    function isTouchEnabled() {
+      return ('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0);
+    }
+
+    // Убирает появление меню при ховере на тач-усройствах
+    if (isTouchEnabled()) {
+      const navLinks = nav.querySelectorAll('.nav__li');
+        navLinks.forEach( (li) => {
+        li.classList.add('-touch-');
+        if ( li.querySelector('button') ) {
+          li.querySelector('button').classList.add('-touch-');
+        }
+      });
+
+      const buttons = nav.querySelectorAll('.nav__extend-button');
+        buttons.forEach( (button) => {
+        button.addEventListener('click', function() {
+          this.classList.toggle('-active-');
+          this.nextElementSibling.classList.toggle('-open-');
+        });
+      });
+
+      // document.addEventListener('click', (evt) => {
+      //   console.log(evt.target.closest('.nav__list--extend-wrapper'))
+
+      //   if ( !evt.target.closest('.nav__list--extend-wrapper') ) {
+      //     evt.target.closest('.nav__list--extend-wrapper').classList.toggle('-open-');
+      //     evt.target.closest('.nav__list--extend-wrapper').previousElementSibling.classList.toggle('-active-');
+      //   }
+      // })
     }
   })
 })();
