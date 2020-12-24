8b247da076f4322db5c0c79387d9e63d279c62bd
Ivan Psarew <psarew.ivan@yandex.ru>
2020-12-25 00:19:34 +0300

    Добавляет хеш в имена картинок

--- webpack.config.js
+++ webpack.config.js
@@ -179,7 +179,7 @@ module.exports = {
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name].[ext]',
+            name: '[name].[contenthash].[ext]',
             outputPath: 'img'
           }
         }
