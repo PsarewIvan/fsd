a0d9d77370a565794c08ab67427b9317ee352945
Ivan Psarew <psarew.ivan@yandex.ru>
2021-03-20 20:17:15 +0300

    Переписывае тлогику на классы

--- src/pug/includes/nav/nav.js
+++ src/pug/includes/nav/nav.js
@@ -1,56 +1,57 @@
-(function () {
-  const navigation = document.querySelectorAll('.nav');
-
-  navigation.forEach((nav) => {
-    // Убирает атрибут href у текущей ссылки
-    if (nav.querySelector('.nav__link--current')) {
-      const navLink = nav.querySelector('.nav__link--current');
-      navLink.removeAttribute('href');
-    }
-
-    // Проверка на тач-устройства
-    function isTouchEnabled() {
-      return (
-        'ontouchstart' in window ||
-        navigator.maxTouchPoints > 0 ||
-        navigator.msMaxTouchPoints > 0
-      );
-    }
-
-    // Убирает появление меню при ховере на тач-устройствах
-    if (isTouchEnabled()) {
-      const navLinks = nav.querySelectorAll('.nav__li');
-      const buttons = nav.querySelectorAll('.nav__extend-button');
-
-      function toggleMenu(button) {
-        button.classList.toggle('-active-');
-        button.nextElementSibling.classList.toggle('-open-');
-      }
-
-      function createOverlay() {
-        const overlay = document.createElement('div');
-        overlay.classList.add('nav__list-overlay');
-        return overlay;
-      }
-
-      navLinks.forEach((li) => {
-        li.classList.add('-touch-');
-        if (li.querySelector('button')) {
-          li.querySelector('button').classList.add('-touch-');
-        }
+class Nav {
+  constructor(nav) {
+    this.nav = nav;
+    this.MIN_WINDOW_WIDTH = 980;
+    this.navLists = this.nav.querySelectorAll('.js-nav__li');
+    this.exLinks = this.nav.querySelectorAll('.js-nav__extend-link');
+    this.clearCurrentLink();
+    if (this.isTouchEnabled || !this.isWindowWide) {
+      this.navLists.forEach((li) => {
+        this.toggleClassMod(li);
       });
-
-      buttons.forEach((button) => {
-        button.addEventListener('click', function () {
-          const overlay = createOverlay();
-          toggleMenu(button);
-          document.body.append(overlay);
-          overlay.addEventListener('click', () => {
-            toggleMenu(button);
-            overlay.remove();
-          });
-        });
+      this.exLinks.forEach((exLink) => {
+        this.exLinkListener(exLink);
       });
     }
-  });
-})();
+  }
+
+  clearCurrentLink() {
+    const navLink = this.nav.querySelector('.nav__link--current');
+    navLink ? navLink.removeAttribute('href') : null;
+  }
+
+  exLinkListener(exLink) {
+    exLink.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      this.toggleMenu(exLink);
+      exLink.blur();
+    });
+  }
+
+  toggleMenu(exLink) {
+    exLink.classList.toggle('-active-');
+    exLink.nextElementSibling.classList.toggle('-open-');
+  }
+
+  toggleClassMod(li) {
+    li.classList.toggle('-touch-');
+    const exLink = li.querySelector('.js-nav__extend-link');
+    exLink ? exLink.classList.toggle('-touch-') : null;
+  }
+
+  get isTouchEnabled() {
+    return (
+      'ontouchstart' in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    );
+  }
+
+  get isWindowWide() {
+    return document.documentElement.clientWidth > this.MIN_WINDOW_WIDTH;
+  }
+}
+
+document.querySelectorAll('.js-nav').forEach((nav) => {
+  new Nav(nav);
+});
