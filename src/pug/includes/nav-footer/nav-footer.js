8a2642436a072aacee26b2a17043183bd81a87e0
Ivan Psarew <psarew.ivan@yandex.ru>
2021-03-21 22:19:19 +0300

    Создает логику меню футера

---
+++ src/pug/includes/nav-footer/nav-footer.js
@@ -0,0 +1,76 @@
+class NavFooter {
+  constructor(nav) {
+    this.nav = nav;
+    this.WINDOW_WIDTH_THIN = 870;
+    this.isActive = false;
+    this.menu = nav.querySelector('.js-nav-footer__nav');
+    if (document.documentElement.clientWidth <= this.WINDOW_WIDTH_THIN) {
+      this.activateMenu();
+    }
+    this.windowListener();
+  }
+
+  buttonListener() {
+    const button = this.nav.querySelector('.js-nav-footer__title');
+    button.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      button.blur();
+      button.classList.toggle('-open-');
+      this.menu.classList.toggle('-open-');
+    });
+  }
+
+  windowListener() {
+    window.addEventListener('resize', (evt) => {
+      if (evt.target.innerWidth <= this.WINDOW_WIDTH_THIN && !this.isActive) {
+        this.activateMenu();
+      }
+      if (evt.target.innerWidth > this.WINDOW_WIDTH_THIN && this.isActive) {
+        this.deactivateMenu();
+      }
+    });
+  }
+
+  activateMenu() {
+    this.replaceTag();
+    this.buttonListener();
+    this.isActive = true;
+  }
+
+  deactivateMenu() {
+    this.replaceTag();
+    this.isActive = false;
+  }
+
+  replaceTag() {
+    const pageElement = this.nav.querySelector('.js-nav-footer__title');
+    let insertElement;
+    if (!this.isActive) {
+      insertElement = this.createButton();
+    }
+    if (this.isActive) {
+      insertElement = this.createHeading();
+    }
+    insertElement.innerHTML = pageElement.innerHTML;
+    pageElement.after(insertElement);
+    pageElement.remove();
+  }
+
+  createButton() {
+    const button = document.createElement('button');
+    button.classList.add('nav-footer__title', 'js-nav-footer__title');
+    button.setAttribute('type', 'button');
+    button.setAttribute('aria-label', 'Открыть меню');
+    return button;
+  }
+
+  createHeading() {
+    const heading = document.createElement('h4');
+    heading.classList.add('nav-footer__title', 'js-nav-footer__title');
+    return heading;
+  }
+}
+
+document.querySelectorAll('.nav-footer').forEach((nav) => {
+  new NavFooter(nav);
+});
