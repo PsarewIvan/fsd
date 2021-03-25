b1c2728b02f6f4fec9e953d68052402312848735
Ivan Psarew <psarew.ivan@yandex.ru>
2021-03-25 22:53:46 +0300

    Правит код слайдера картинок

--- src/pug/includes/img-slider/img-slider.js
+++ src/pug/includes/img-slider/img-slider.js
@@ -1,12 +1,14 @@
 import 'slick-carousel';
 
-(function() {
+(() => {
   $(document).ready(function () {
     $('.img-slider').slick({
       dots: true,
       infinite: true,
-      nextArrow: "<button type='button' class='slick-next' aria-label='Next'></button>",
-      prevArrow: "<button type='button' class='slick-prev' aria-label='Previous'></button>",
+      nextArrow:
+        "<button type='button' class='slick-next' aria-label='Next'></button>",
+      prevArrow:
+        "<button type='button' class='slick-prev' aria-label='Previous'></button>",
     });
   });
 })();
