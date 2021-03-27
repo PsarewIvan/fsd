870c01b121f51a372bec58697278bd085f383e14
Ivan Psarew <psarew.ivan@yandex.ru>
2021-03-27 17:50:00 +0300

    Добавляет  js-классы для слайдера цены

--- src/pug/includes/slider/slider.js
+++ src/pug/includes/slider/slider.js
@@ -3,7 +3,7 @@ import 'ion-rangeslider';
 (() => {
   const sliders = document.querySelectorAll('.js-slider');
   sliders.forEach((slider) => {
-    const fromToValue = slider.querySelector('.slider__data-from-to');
+    const fromToValue = slider.querySelector('.js-slider__data-from-to');
     function changeValue(data) {
       $(fromToValue).html(
         `${data.from_pretty}&#8381; - ${data.to_pretty}&#8381;`
