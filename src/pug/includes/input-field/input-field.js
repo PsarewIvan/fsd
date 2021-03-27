e010515f074d9aa57309d7d603ec5a949d49a6f9
Ivan Psarew <psarew.ivan@yandex.ru>
2021-03-27 17:48:20 +0300

    Добавляет js-классы для поле ввода

--- src/pug/includes/input-field/input-field.js
+++ src/pug/includes/input-field/input-field.js
@@ -1,7 +1,7 @@
 import IMask from 'imask';
 
 (() => {
-  const elements = document.querySelectorAll('.input-field__input--masked');
+  const elements = document.querySelectorAll('.js-input-field__input--masked');
   elements.forEach((element) => {
     const maskOptions = {
       mask: Date,
