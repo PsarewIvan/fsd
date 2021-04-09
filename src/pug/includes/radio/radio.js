0d08de594032035d6cf709fc87bfd96079d25932
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-09 22:38:27 +0300

    Добавляет логику присвоения модификаторов, правит классы в radio

---
+++ src/pug/includes/radio/radio.js
@@ -0,0 +1,45 @@
+class Radio {
+  constructor(radio) {
+    this.LABEL_CHECKED_CLASS = 'radio__label--checked';
+    this.LABEL_FOCUS_CLASS = 'radio__label--focus';
+    this.HIDDEN_LIST_CLASS = 'radio__list--hidden';
+    this.radio = radio;
+    this.list = radio.querySelector('.js-radio__list');
+    this.inputs = radio.querySelectorAll('.js-radio__input');
+    this.labels = radio.querySelectorAll('.js-radio__label');
+
+    this.inputs.forEach((input, i) => {
+      this.labelCheckedToggle(this.labels[i], input);
+      input.addEventListener('focus', this.inputFocusHandler.bind(this, i));
+      input.addEventListener('blur', this.inputBlurHandler.bind(this, i));
+    });
+  }
+
+  inputFocusHandler(i) {
+    this.labels.forEach((label, index) => {
+      if (i === index) {
+        label.classList.add(this.LABEL_FOCUS_CLASS);
+        label.classList.add(this.LABEL_CHECKED_CLASS);
+      } else {
+        label.classList.remove(this.LABEL_FOCUS_CLASS);
+        label.classList.remove(this.LABEL_CHECKED_CLASS);
+      }
+    });
+  }
+
+  inputBlurHandler(i) {
+    this.labels[i].classList.remove(this.LABEL_FOCUS_CLASS);
+  }
+
+  labelCheckedToggle(label, input) {
+    if (input.checked) {
+      label.classList.add(this.LABEL_CHECKED_CLASS);
+    } else {
+      label.classList.remove(this.LABEL_CHECKED_CLASS);
+    }
+  }
+}
+
+document.querySelectorAll('.js-radio').forEach((radio) => {
+  new Radio(radio);
+});
