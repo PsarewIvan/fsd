384bd1551567fdc3fab713230074b7231c19242b
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-05 19:52:54 +0300

    Правит написание кавычек

--- src/pug/includes/img-slider/ImgSlider.js
+++ src/pug/includes/img-slider/ImgSlider.js
@@ -1,12 +1,19 @@
 import 'slick-carousel';
 
-(() => {
-  $(document).ready(function () {
-    $('.js-img-slider').slick({
+class ImgSlider {
+  constructor() {
+    this.SLIDER_CLASS = 'js-img-slider';
+    this.$root = $(`.${this.SLIDER_CLASS}`);
+  }
+
+  init() {
+    this.$root.slick({
       dots: true,
       infinite: true,
       nextArrow: `<button type='button' class='slick-next' aria-label='Next'></button>`,
       prevArrow: `<button type='button' class='slick-prev' aria-label='Previous'></button>`,
     });
-  });
-})();
+  }
+}
+
+export default ImgSlider;
